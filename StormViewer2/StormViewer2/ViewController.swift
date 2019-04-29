ac4935c29dde0844ab5f1be97c65d760ac7c6921
ignasiTest <test@ignasiswift.com>
2019-04-29 19:55:43 +0200

    PART 3

---
+++ StormViewer2/StormViewer2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StormViewer2
+//
+//  Created by Ignasi Perez  on 29/04/2019.
+//  Copyright © 2019 Ignasi Perez . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
